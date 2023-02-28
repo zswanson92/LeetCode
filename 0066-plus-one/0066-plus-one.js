0f81ce9cef3ed4ee8ea25f695f4521c04b6138a9
zswan <68711430+zswanson92@users.noreply.github.com>
2023-02-28 11:02:31 -0600

    Time: 65 ms (58.08%), Space: 41.9 MB (66.91%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -4,18 +4,10 @@
  */
 var plusOne = function(digits) {
     let abc = digits.join('')
-    let num = Number(abc) + 1
-    console.log(num)
+    
     
     let bigIntNum = BigInt(abc) + BigInt(1)
-    console.log("!!!!!", bigIntNum)
     let numSplit = String(bigIntNum).split('')
-    for(let i = numSplit.length; i > 0; i--){
-        if(numSplit[i] === '9'){
-            numSplit[i - 1]++
-        } else {
-            break
-        }
-    }
+    
     return numSplit
 };
