3bda89b6a0a2ca1cc93f100baca10aab4e66239c
zswan <68711430+zswanson92@users.noreply.github.com>
2023-03-06 17:33:45 -0600

    Time: 68 ms (58.21%), Space: 44.1 MB (15.64%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
@@ -4,19 +4,13 @@
  */
 var findGCD = function(nums) {
     let sorted = nums.sort((a, b) => a - b)
-    console.log(sorted)
     let smallest = sorted[0]
     let largest = sorted[sorted.length - 1]
-    console.log(smallest)
-    console.log(largest)
-    
-    
     for(let i = largest; i >= 0; i--){
         if(largest % i === 0 && smallest % i === 0){
             return i
         }
     }
-    
     
     return 1
 };
