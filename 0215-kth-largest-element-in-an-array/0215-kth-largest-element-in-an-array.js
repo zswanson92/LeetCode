8f97ee5c7f7cc349d9833299bec3e7aaf08b9ac2
zswan <68711430+zswanson92@users.noreply.github.com>
2023-03-14 15:24:06 -0500

    Time: 176 ms (54.30%), Space: 52.1 MB (28.85%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -7,8 +7,5 @@
 var findKthLargest = function(nums, k) {
     if(nums.length === 1) return nums[0]
     let sorted = nums.sort((a, b) => b - a)
-    console.log(sorted)
-    console.log(sorted.indexOf(k - 1))
-    // if(k > 1) return sorted.indexOf(k - 1)
     return sorted[k - 1]
 };
