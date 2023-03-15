88ef0edf7503bd3da666a9d5369976311c3c19a2
zswan <68711430+zswanson92@users.noreply.github.com>
2023-03-15 16:13:17 -0500

    Time: 198 ms (7.62%), Space: 45.4 MB (9.46%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -7,7 +7,6 @@ var removeDuplicates = function(nums) {
     
     for(let i = 0; i < nums.length; i++){
         if(nums[i] === nums[i + 1] && nums[i] !== '_'){
-            console.log(nums)
             nums.push('_')
             nums.splice(i, 1)
             i--
