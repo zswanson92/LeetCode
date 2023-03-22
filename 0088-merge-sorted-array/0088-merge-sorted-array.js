ee8510ed5dd00e64761f564c8112d391a1ad7cef
zswan <68711430+zswanson92@users.noreply.github.com>
2023-03-22 18:25:04 -0500

    Time: 62 ms (63.28%), Space: 41.8 MB (76.02%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,34 +6,6 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-//     if(m === 0){
-//         nums1 = [0]
-//     }
-//     if(nums1.length !== m){
-//        nums1.splice(nums1.length - m, m) 
-//     }
-    
-//     if(n === 0){
-//         nums2 = []
-//     }
-//     if(nums2.length !== n){
-//         nums2.splice(nums2.length - n, n)
-//     }
-    
-    
-    
-    
-//     if(nums1.length > 1){
-//         nums1.push(...nums2) 
-//     } else {
-//         nums1.push(...nums2)
-//         nums1.splice(0,1)
-//     }
-    
-//     console.log(nums1)
-//     console.log(nums2)
-    
-//     nums1.sort((a,b) => a - b)
     let insert = m + n - 1
     m--
     n--
