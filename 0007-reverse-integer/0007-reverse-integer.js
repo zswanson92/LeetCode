353f692c21208a8e7190267cd02da70067d88e9c
zswan <68711430+zswanson92@users.noreply.github.com>
2023-03-23 16:25:03 -0500

    Time: 78 ms (47.12%), Space: 44.1 MB (25.77%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var reverse = function(x) {
-    // console.log(x.toString().split(''))
     let xyz;
     let abc = x.toString().split('')
     if(!abc.includes('-')){
@@ -15,14 +14,10 @@ var reverse = function(x) {
         abc.splice(0, 1)
         abc.reverse()
         abc.unshift('-')
-        console.log("!!!!", abc)
         xyz = abc.join('')
-        console.log("???", xyz)
     }
     
     xyz = Number(xyz)
-    console.log(typeof xyz)
-    // abc = Number(abc)
     
     if(xyz > -(2**31) && xyz < 2**31 - 1){
        return xyz 
