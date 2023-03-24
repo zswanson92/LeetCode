b688b1c03d2819a3520a90c332d1a346cad2b89c
zswan <68711430+zswanson92@users.noreply.github.com>
2023-03-24 14:57:13 -0500

    Time: 65 ms (66.85%), Space: 44.3 MB (42.66%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var mostWordsFound = function(sentences) {
-    let max = 0
     for(let i = 0; i < sentences.length; i++){
         sentences[i] = sentences[i].split(' ')
     }
