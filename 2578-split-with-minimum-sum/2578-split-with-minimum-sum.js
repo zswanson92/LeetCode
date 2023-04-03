a65357f7b5887842904becd3a1043742f6a6de4e
zswan <68711430+zswanson92@users.noreply.github.com>
2023-04-03 15:37:09 -0500

    Time: 59 ms (56.54%), Space: 42.3 MB (43.46%) - LeetHub

--- 2578-split-with-minimum-sum/2578-split-with-minimum-sum.js
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.js
@@ -7,13 +7,10 @@ var splitNum = function(num) {
     let ptTwo = "";
     let abc = num.toString()
     let arr = []
-    console.log(abc.split(''))
     for(let i = 0; i < abc.length; i++){
         arr.push(Number(abc[i]))
     }
-    console.log(abc)
     arr.sort((a, b) => a - b)
-    console.log(arr)
     for(let j = 0; j < arr.length; j++){
         if(j % 2 === 0){
             ptOne += arr[j]
