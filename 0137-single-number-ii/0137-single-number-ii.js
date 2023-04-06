6cbb9390fb934a36af1081af6039a10692d9d24d
zswan <68711430+zswanson92@users.noreply.github.com>
2023-04-06 16:03:12 -0500

    Time: 63 ms (75.00%), Space: 42.6 MB (81.50%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.js
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -4,11 +4,7 @@
  */
 var singleNumber = function(nums) {
     nums.sort((a, b) => a - b)
-    // console.log(nums)
     for(let i = 0; i < nums.length; i += 3){
-        // console.log(abc[i])
-        // console.log(abc[i + 2])
-        // console.log("THIS IS I", i)
         if(nums[i] !== nums[i + 2]){
             return nums[i]
         }
