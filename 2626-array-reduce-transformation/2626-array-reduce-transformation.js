e5fcb1af2a2a0bb548e2769bb51e04c9f38e20b9
zswan <68711430+zswanson92@users.noreply.github.com>
2023-04-14 12:32:05 -0500

    Time: 74 ms (5.06%), Space: 44.5 MB (15.73%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    
+//     let res = init
+//     for(let i = 0; i < nums.length; i++){
+//         res = fn(res, i)
+//     }
+//     return res
+    
+    let ans = init;
+    for (let i of nums)
+        ans = fn(ans, i);
+    
+    return ans;
+    
+    
+    
+    
+};
