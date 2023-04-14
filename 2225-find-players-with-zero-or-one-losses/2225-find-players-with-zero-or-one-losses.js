ca9d19bc1316162479fa0f60875a295ac4db0c9f
zswan <68711430+zswanson92@users.noreply.github.com>
2023-04-14 14:21:45 -0500

    Time: 1201 ms (6.00%), Space: 85.2 MB (48.40%) - LeetHub

---
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[][]} matches
+ * @return {number[][]}
+ */
+var findWinners = function(matches) {
+    let res = []
+    let loser = {}
+    let abc = new Set()
+    let loseArr = []
+    
+    for(let i = 0; i < matches.length; i++){
+        let curr = matches[i]
+        for(let j = 0; j < curr.length; j++){
+            if(j === 0){
+                abc.add(curr[j])
+            } else if(!loser[curr[j]]){
+                loser[curr[j]] = 1
+            } else{
+                loser[curr[j]]++
+            }
+        }
+    }
+
+    for(let key in loser){
+        console.log(typeof Number(key))
+        if(abc.has(Number(key))){
+            abc.delete(Number(key))
+        }
+        if(loser[key] === 1){
+            loseArr.push(Number(key))
+        }
+    }
+    let xyz = Array.from(abc)
+    xyz.sort((a, b) => a - b)
+    res.push(xyz)
+    loseArr.sort((a, b) => a - b)
+    res.push(loseArr)
+    return res
+};
