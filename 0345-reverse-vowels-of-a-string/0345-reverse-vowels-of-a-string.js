e5bbd89653eac3e3da84e7d1729b88db1501bf61
zswan <68711430+zswanson92@users.noreply.github.com>
2023-05-08 15:46:01 -0500

    Time: 95 ms (20.39%), Space: 48.9 MB (50.28%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -3,19 +3,14 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
-    // let obj = {}
     let arr = []
     let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
     for(let i = 0; i < s.length; i++){
         if(vowels.includes(s[i])){
-            // obj[i] = s[i]
             arr.push(s[i])
         }
     }
-    
     s = s.split('')
-    console.log(s)
-    console.log(arr)
     let count = 0
     for(let g = s.length; g >= 0; g--){
         if(vowels.includes(s[g])){
@@ -24,6 +19,5 @@ var reverseVowels = function(s) {
         }
     }
     
-    console.log(s)
     return s.join('')
 };
