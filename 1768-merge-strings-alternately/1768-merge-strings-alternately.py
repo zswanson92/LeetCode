141754433882bd041f1c5d9fcacc2bd6f1409406
zswan <68711430+zswanson92@users.noreply.github.com>
2023-05-08 12:08:29 -0500

    Time: 49 ms (5.11%), Space: 16.4 MB (5.20%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,9 +1,8 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
        
-        longest = len(word1) if len(word1) > len(word2) else len(word2)
         res = ""
-        for x in range(longest):
+        for x in range(max(len(word1), len(word2))):
             if not x > len(word1) - 1:
                 res += word1[x]
             if not x > len(word2) - 1:
