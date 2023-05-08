2ac082aba5f7a817e004e9dae1ebf0b5cc279499
zswan <68711430+zswanson92@users.noreply.github.com>
2023-05-08 15:11:24 -0500

    Time: 83 ms (6.90%), Space: 46.4 MB (6.96%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    let avail = 0
+    
+    for(let i = 0; i < flowerbed.length; i++){
+        if((flowerbed[i] === 0 && flowerbed[i - 1] !== 1 && flowerbed[i + 1] === 0) || (flowerbed[i] === 0 && flowerbed[i - 1] !== 1 && flowerbed[i + 1] === undefined)){
+            avail += 1
+            flowerbed[i] = 1
+        }
+    }
+    
+    console.log(flowerbed)
+    console.log(avail)
+    
+    return avail >= n
+};
