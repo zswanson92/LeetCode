08df8993ec16f6f1b8e1d65f2fc0a468fd580340
zswan <68711430+zswanson92@users.noreply.github.com>
2023-05-08 12:42:07 -0500

    Time: 55 ms (87.46%), Space: 42.2 MB (76.04%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+
+  if (str1 + str2 !== str2 + str1) {
+    return '';
+  }
+
+  const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+  const len = gcd(str1.length, str2.length);
+
+  return str1.substring(0, len);
+};
