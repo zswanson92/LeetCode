1751ac98fef86e28e7753d5c9d7ec6f2bc1271ee
zswan <68711430+zswanson92@users.noreply.github.com>
2023-05-10 10:38:52 -0500

    Time: 57 ms (5.21%), Space: 16.4 MB (11.23%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        j = 0
+        if len(s) == 0:
+            return True
+        for i in range(len(t)):
+            if len(s) > 0 and t[i] == s[j]:
+                j += 1
+                if j == len(s):
+                    return True
+        
+        return False
+        
+        
+        
+        
+
+        
+        
+        
